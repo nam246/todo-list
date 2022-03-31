310abbf1407e31d0254a2d58ba72037315317829
nam246 <60662102+nam246@users.noreply.github.com>
2022-04-01 00:14:36 +0700

    initial commit

---
+++ src/components/Button.vue
@@ -0,0 +1,28 @@
+<template>
+    <button @click="onClick()" :style="{background: color}">{{ text }}</button>
+</template>
+
+<script>
+export default {
+    name: 'Button',
+    props: {
+        text: String,
+        color: String,
+    },
+    methods: {
+        onClick() {
+            this.$emit('toggle-add-task')
+        }
+    },
+}
+</script>
+
+<style scoped>
+    button {
+        border-radius: 10px;
+        padding: 10px 20px;
+        color: #fff;
+        border: none;
+        cursor: pointer;
+    }
+</style>
