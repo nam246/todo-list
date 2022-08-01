e110ebbc5e908a535e94ed7387d15a39a578274b
nam246 <60662102+nam246@users.noreply.github.com>
2022-08-01 22:15:10 +0700

    update config

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  publicPath: '/my1st-todolist/',
+  publicPath: '/todo-list/',
   transpileDependencies: true
 })
